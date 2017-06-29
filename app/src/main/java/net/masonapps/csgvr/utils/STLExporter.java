c0891fdda3436edd98652ec5a97ea906fefd180b
bobm3 <mason94275@gmail.com>
2017-06-29 11:43:54 -0400

    added stl exporting

--- app/src/main/java/net/masonapps/csgvr/utils/STLExporter.java
+++ app/src/main/java/net/masonapps/csgvr/utils/STLExporter.java
@@ -1,5 +1,6 @@
 package net.masonapps.csgvr.utils;
 
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.FloatArray;
 import com.badlogic.gdx.utils.ShortArray;
@@ -10,6 +11,7 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.geometry.partitioning.RegionFactory;
 
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,6 +28,7 @@ public class STLExporter {
     private final float[] vertices;
     private final short[] indices;
     private final int vertexSize;
+    public Matrix4 transform = new Matrix4();
 
     public STLExporter(float[] vertices, short[] indices, int vertexSize) {
         this.vertices = vertices;
@@ -84,10 +87,10 @@ public class STLExporter {
             int i1 = indices[i] * vertexSize;
             int i2 = indices[i + 1] * vertexSize;
             int i3 = indices[i + 2] * vertexSize;
-            v1.set(vertices[i1], vertices[i1 + 1], vertices[i1 + 2]);
-            v2.set(vertices[i2], vertices[i2 + 1], vertices[i2 + 2]);
-            v3.set(vertices[i3], vertices[i3 + 1], vertices[i3 + 2]);
-            normal.set(v2).sub(v1).crs(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z).nor();
+            v1.set(vertices[i1], vertices[i1 + 1], vertices[i1 + 2]).mul(transform);
+            v2.set(vertices[i2], vertices[i2 + 1], vertices[i2 + 2]).mul(transform);
+            v3.set(vertices[i3], vertices[i3 + 1], vertices[i3 + 2]).mul(transform);
+            normal.set(v2).sub(v1).crs(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z).rot(transform).nor();
 
             writer.write(String.format(Locale.US, "facet normal %f %f %f", normal.x, normal.y, normal.z));
             writer.newLine();
@@ -115,8 +118,47 @@ public class STLExporter {
         writer.close();
     }
 
-    private void writeBinary(OutputStream outputStream) {
+    private void writeBinary(OutputStream outputStream) throws IOException {
+        final DataOutputStream stream = new DataOutputStream(outputStream);
 
+        for (int i = 0; i < 80; i++) {
+            stream.writeByte(0);
+        }
+
+        stream.write(indices.length / 3);
+
+        final Vector3 v1 = new Vector3();
+        final Vector3 v2 = new Vector3();
+        final Vector3 v3 = new Vector3();
+        final Vector3 normal = new Vector3();
+
+        for (int i = 0; i < indices.length; i += 3) {
+            int i1 = indices[i] * vertexSize;
+            int i2 = indices[i + 1] * vertexSize;
+            int i3 = indices[i + 2] * vertexSize;
+            v1.set(vertices[i1], vertices[i1 + 1], vertices[i1 + 2]);
+            v2.set(vertices[i2], vertices[i2 + 1], vertices[i2 + 2]);
+            v3.set(vertices[i3], vertices[i3 + 1], vertices[i3 + 2]);
+            normal.set(v2).sub(v1).crs(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z).nor();
+
+            stream.writeFloat(normal.x);
+            stream.writeFloat(normal.y);
+            stream.writeFloat(normal.z);
+
+            stream.writeFloat(v1.x);
+            stream.writeFloat(v1.y);
+            stream.writeFloat(v1.z);
+
+            stream.writeFloat(v2.x);
+            stream.writeFloat(v2.y);
+            stream.writeFloat(v2.z);
+
+            stream.writeFloat(v3.x);
+            stream.writeFloat(v3.y);
+            stream.writeFloat(v3.z);
+
+            stream.writeShort(0);
+        }
     }
 
     public enum FileType {
